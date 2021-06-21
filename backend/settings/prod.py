16062551cbf4237d97b94549cf3797efdfec4848
Kieran <kcosson111@gmail.com>
2021-06-21 11:38:59 +0100

    Setup Heroku

---
+++ backend/settings/prod.py
@@ -0,0 +1,25 @@
+""" Production Settings """
+
+import os
+import dj_database_url
+from .dev import *
+
+############
+# DATABASE #
+############
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL')
+    )
+}
+
+############
+# SECURITY #
+############
+
+DEBUG = bool(os.getenv('DJANGO_DEBUG', ''))
+
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', SECRET_KEY)
+
+ALLOWED_HOSTS = ['*']
