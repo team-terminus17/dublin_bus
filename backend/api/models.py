3d5fb85ee990b74fcc6b6017a4ea661882e78523
ChristineCao98 <xiaoyan.cao@ucdconnect.ie>
2021-06-29 13:53:11 +0100

    basic backend apis in place

--- backend/api/models.py
+++ backend/api/models.py
@@ -11,17 +11,26 @@ class Stops(models.Model):
     class Meta:
         db_table = 'stops'
 
+    def dictify(self):
+        """Return the model as a dictionary"""
+        stop = dict()
+        stop['Name'] = self.stopName
+        stop['lon'] = self.lon
+        stop['lat'] = self.lat
+        return stop
+
     def __str__(self):
         return str(self.stopID) + self.stopName
 
 
 class RouteStops(models.Model):
-    routeID = models.CharField(max_length=10)
+    routeID = models.CharField(primary_key=True, max_length=10)
     direction = models.CharField(max_length=1)
     stopID = models.IntegerField()
+    sequence = models.IntegerField()
 
     class Meta:
-        db_table = 'route_stops'
+        db_table = 'routestops'
         unique_together = ("routeID", "direction", "stopID")
 
 
@@ -33,3 +42,10 @@ class Weather(models.Model):
 
     class Meta:
         db_table = 'futureweather'
+
+    def dictify(self):
+        weather = dict()
+        weather['temp'] = self.temp
+        weather['rain'] = self.rain
+        weather['icon'] = self.icon
+        return weather
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,8 @@
 from django.views.generic import TemplateView
 from django.views.decorators.cache import never_cache
-from django.http import HttpResponse
-from .models import Stops
+from django.http import HttpResponse, JsonResponse
+from .models import *
+import json
 
 # Serve Vue Application
 index_view = never_cache(TemplateView.as_view(template_name='index.html'))
@@ -14,5 +15,54 @@ def test_message(request):
 
 def test_db(request):
     """Dummy message to check if the database is integrated successfully"""
-    stop = Stops.objects.all();
-    return HttpResponse(stop)
+    stops = RouteStops.objects.all()
+    return HttpResponse(stops)
+
+
+def get_routes(request):
+    """Return json of available bus routes"""
+    with open('scrapers/routes.json', 'r') as file:
+        return JsonResponse(json.load(file))
+
+
+def get_stops(request, stop):
+    """Return json of available bus stops for a single route"""
+    route_dict = {}
+    with open('scrapers/stops_by_route.json', 'r') as file:
+        route_dict = json.load(file)
+    stop_list = route_dict['routes'][stop]
+    route_dict_detail = {}
+    if len(stop_list['inbound']) != 0:
+        route_dict_detail['inbound'] = {}
+        for s in stop_list['inbound']:
+            route_dict_detail['inbound'][s] = Stops.objects.get(stopID=int(s)).dictify()
+    if len(stop_list['outbound']) != 0:
+        route_dict_detail['outbound'] = {}
+        for s in stop_list['outbound']:
+            route_dict_detail['outbound'][s] = Stops.objects.get(stopID=int(s)).dictify()
+    return JsonResponse(route_dict_detail)
+
+
+def predict_time(request, route, direction, arr_stop, dep_stop, datetime):
+    """Take a route, two stops indices, a datetime. Return a predicted time taken."""
+    direction_full = 'inbound' if direction == 'I' else 'outbound'
+    with open('scrapers/stops_by_route.json', 'r') as file:
+        route_dict = json.load(file)
+    dep_stop_seq = RouteStops.objects.get(routeID=route, direction=direction, stopID=dep_stop).sequence
+    arr_stop_seq = RouteStops.objects.get(routeID=route, direction=direction, stopID=arr_stop).sequence
+    stop_list = route_dict['routes'][route][direction_full][dep_stop_seq:arr_stop_seq+1]
+    # First we need to find the predicted weather of the time closet to the input datetime.
+    # If it exceeds the limit of the prediction, we just get current weather info
+    weather = Weather.objects.filter(weather_time__gte=datetime).order_by('weather_time')
+    if len(weather) == 0:
+        weather = Weather.objects.all().order_by('weather_time')
+    weather = weather[0]
+    stop_ret = {'stops': stop_list, 'weather': weather.dictify()}
+    return JsonResponse(stop_ret)
+
+
+def get_weather(request):
+    """Return weather information of current time"""
+    weather = Weather.objects.all().order_by('weather_time')[0]
+    return JsonResponse(weather.dictify())
+
