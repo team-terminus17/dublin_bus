542e1f3d2d2afe4d5c6aba4c7a2a8f0173a84a8e
ChristineCao98 <xiaoyan.cao@ucdconnect.ie>
2021-07-05 17:40:58 +0100

    map component added

---
+++ src/components/Map.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+<!--:center="{lat: 53.3673893, lng: -6.2600157}"-->
+    <div id="map"
+    style="width: 100%;height: 500px"
+    >
+    </div>
+  </div>
+</template>
+
+<script>
+// import DirectionsRenderer from "./DirectionsRenderer";
+export default {
+  name: "Map",
+  data(){
+    return{
+      map:null,
+    }
+  },
+  mounted() {
+    window.checkAndAttachMapScript(this.initMap)
+  },
+  methods:{
+    initMap(){
+      this.map= new window.google.maps.Map(document.getElementById("map"),{
+        center: {lat: 53.3673893, lng: -6.2600157},
+        zoom: 12,
+      });
+    }
+  }
+}
+</script>
