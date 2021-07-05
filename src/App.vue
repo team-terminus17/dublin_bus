54e32a04cbbae7978e11517efdccffcfbc32586d
Kieran <kcosson111@gmail.com>
2021-07-05 17:42:21 +0100

    Experiment with <svg>

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,28 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-    <div class="dropdown">
-      <button
-        class="btn btn-secondary dropdown-toggle"
-        type="button"
-        id="dropdownMenuButton1"
-        data-bs-toggle="dropdown"
-        aria-expanded="false"
-      >
-        Check Bootstrap
-      </button>
-      <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-        <li><a class="dropdown-item" href="#">Action</a></li>
-        <li><a class="dropdown-item" href="#">Another action</a></li>
-        <li><a class="dropdown-item" href="#">Something else here</a></li>
-      </ul>
-    </div>
+    <time-picker></time-picker>
   </div>
 </template>
+
+<script lang="ts">
+
+
+import { Component, Vue } from 'vue-property-decorator';
+import TimePicker from '@/components/TimePicker.vue'; // @ is an alias to /src
+
+@Component({
+  components: {
+    TimePicker,
+  },
+})
+export default class App extends Vue {}
+
+</script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
--- src/components/TimePicker.vue
+++ src/components/TimePicker.vue
@@ -1,11 +1,45 @@
 <template>
-$END$
+  <svg height="8.5em" width="8.5em">
+    <rect width="100%" height="100%" style="fill:red"></rect>
+      <rect
+        v-for="shape in shapes"
+        :key="shape.len"
+        :x="shape.pos"
+        :y="shape.pos"
+        :width="shape.len"
+        :height="shape.len"
+        :style="shape.style"
+    ></rect>
+  </svg>
 </template>
 
-<script>
-export default {
-name: "TimePicker"
+<script lang="ts">
+
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+class Shape {
+  pos: string
+  len: string
+  style: string
+  constructor(pos: string, len: string, color: string) {
+    this.pos = pos + "%";
+    this.len = len + "%";
+    this.style = "fill:" + color;
+  }
 }
+
+@Component
+export default class TimePicker extends Vue {
+  get shapes(): Shape[] {
+    return [
+      new Shape("5", "90", "blue"),
+      new Shape("10", "80", "yellow"),
+      new Shape("15", "70", "red")
+    ]
+  }
+}
+
 </script>
 
 <style scoped>
