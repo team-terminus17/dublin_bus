2e42bdc4215adb315c8ec8b11f504c99f117a648
ChristineCao98 <xiaoyan.cao@ucdconnect.ie>
2021-08-19 01:07:23 +0100

    Add instruction box

--- src/components/RealtimeView.vue
+++ src/components/RealtimeView.vue
@@ -1,6 +1,11 @@
 <template>
-  <div>
-    <h2>Real Time</h2>
+  <div style="position: relative">
+    <div id="realtime_header">
+    <h2>Real Time
+      <img src="https://img.icons8.com/small/16/ffffff/info.png" id="info_img">
+    </h2>
+    <div v-html="instruction_info" id="track_instruction"></div>
+    </div>
     <div class="route-selection">
       <RouteSelection
           @selectRoute="onRouteSelected"
@@ -36,6 +41,30 @@
   margin-bottom: 1em;
 }
 
+#realtime_header:hover #track_instruction{
+  visibility: visible;
+}
+
+#track_instruction{
+  font-size: small;
+  font-weight: normal;
+  visibility: hidden;
+  /*margin: 20px;*/
+
+  background-color: black;
+  color: #fff;
+  text-align: center;
+  border-radius: 6px;
+  padding: 5px 0;
+  position: absolute;
+  z-index: 1;
+}
+
+#info_img{
+  width: 20px;
+  height: 20px;
+}
+
 </style>
 
 <script>
@@ -57,6 +86,10 @@ export default {
       route: null,
       direction: null,
       stop: null,
+      instruction_info:'<h5>Instructions on tracking</h5>' +
+          '<p>First choose a trip to track, and when you click track button,' +
+          'the app will push a notification when the bus is coming in three minutes</p>' +
+          '<p> Press the button again to stop tracking.</p>'
     };
   },
   methods: {
