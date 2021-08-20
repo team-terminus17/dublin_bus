eded159f064aad78a0e3d9792082d15171037e97
Kieran <kcosson111@gmail.com>
2021-08-20 15:56:42 +0100

    Rework layout to help with mobile view

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,44 @@
 <template>
-<div id="color-mode" :class="mode">
-  <div class="main d-flex flex-column">
-    <DarkMode class="dark-mode-toggle"
+  <div id="color-mode" :class="mode">
+    <div id="main">
+      <DarkMode
+        class="dark-mode-toggle"
         v-on:changeMode="changeMode"
-    ></DarkMode>
-    <div class="content row">
-      <div class="col-sm-6 col-md-6 d-flex
-        justify-content-start">
-        <img src="./assets/team2.png">
+      ></DarkMode>
+      <div id="view-content">
+        <div class="row">
+          <div class="col-sm-6 col-md-6 d-flex justify-content-start">
+            <img id="logo" src="./assets/team2.png" />
+          </div>
+          <div
+            class="col-xs-6 col-sm-6 col-lg-6 d-flex justify-content-end"
+            id="weather"
+          >
+            <Weather></Weather>
+          </div>
+        </div>
+        <div class="row flex-grow-1">
+          <div
+            id="menu"
+            class="d-none d-sm-grid col-md-1 d-flex justify-content-start"
+          >
+            <button
+              v-on:click="onClickViewButton"
+              class="btn btn-warning"
+              style="cursor: pointer"
+            >
+              {{ button_content }}
+            </button>
+          </div>
+          <div class="h-100 col-md-12">
+            <Map></Map>
+          </div>
+        </div>
       </div>
       <div
-      class="
-        col-xs-6
-        col-sm-6
-        col-lg-6
-        d-flex
-        justify-content-end
-        
-      "
-      id="weather"
-    >
-        <Weather></Weather>
-      </div>
-    </div>
-    <div class="content row flex-grow-1">
-      <div id="menu" class="d-none d-sm-grid col-md-1 d-flex justify-content-start">
-        <button v-on:click="onClickViewButton" class="btn btn-warning" style="cursor: pointer;">
-          {{button_content}}
-        </button>
-      </div>
-      <div v-show="isShow" id="input" class="col-xs-4 col-sm-5 col-md-5 col-lg-3">
+        v-show="isShow"
+        id="input-content"
+      >
         <div class="scroll-wrapper">
           <TripSelection
             v-on:googleQueryComplete="showGooglePrediction"
@@ -38,16 +47,16 @@
           ></TripSelection>
         </div>
       </div>
-      <div class="h-100 col-md-12">
-        <Map></Map>
-      </div>
     </div>
   </div>
-</div>
 </template>
 
 <style lang="scss">
 @import "../node_modules/bootstrap-icons/font/bootstrap-icons.css";
+
+/* 
+  Colors and Theming
+*/
 
 .dark-mode-toggle {
   /* 
@@ -62,32 +71,56 @@
 }
 
 #color-mode.light * {
-  --container-color: #347AA0;
-  --main-border-color: #347AA0;
-  --background-color: #E2E2E2;
-  --border-color: #F3D463;
+  --container-color: #347aa0;
+  --main-border-color: #347aa0;
+  --background-color: #e2e2e2;
+  --border-color: #f3d463;
   --font-color: #000000;
   --counter-font-color: #7a7a7a;
 }
 
 #color-mode.dark * {
-  --main-border-color: #17263C;
+  --main-border-color: #17263c;
   --container-color: #2e4a7a;
   --background-color: #242f3e;
-  --border-color: #D59563;
+  --border-color: #d59563;
   --font-color: #ffffff;
   --counter-font-color: #000000;
 }
 
-#input {
+/*
+  Main page layout
+  (Also includes some input styling)
+*/
+
+#main {
+  background-color: var(--main-border-color);
+  text-align: center;
+  padding-bottom: 0.8em;
+  height: 100vh;
+}
+
+#view-content {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+}
+
+#view-content > .row {
+  margin: 0 !important;
+  padding: 0 !important;
+}
+
+#input-content {
   position: absolute;
-  margin-top: 90px;
-  margin-left: 40px;
+  top: 11em;
+  left: 4em;
+  max-height: calc(100% - 16em);
+  max-width: 25em;
   z-index: 2;
   background-color: var(--container-color);
-  color: #F1ECED;
+  color: #f1eced;
   border-radius: var(--border-radius);
-  max-height: calc(100% - 300px);
   display: flex;
   flex-direction: column;
   justify-content: stretch;
@@ -104,75 +137,38 @@
   z-index: 1;
 }
 
-.content.row {
-  margin: 0 !important;
-  padding: 0 !important;
-}
-
-.main {
-  background-color: var(--main-border-color);
-  text-align: center;
-  height: 100vh;
-  padding-bottom: 0.8em;
-}
-
 #menu {
   position: absolute;
   display: inline-block;
   z-index: 1;
   margin-top: 100px;
-  
-}
-
-.bar {
-  width: 35px;
-  height: 5px;
-  background-color: black;
-  margin: 6px 0;
-}
-
-/* width */
-::-webkit-scrollbar {
-  width: 10px;
-}
-
-/* Track */
-::-webkit-scrollbar-track {
-  background: #f1f1f1;
-  border-radius: var(--border-radius);
-}
-
-/* Handle */
-::-webkit-scrollbar-thumb {
-  background: #888;
-  border-radius: var(--border-radius);
-}
-
-/* Handle on hover */
-::-webkit-scrollbar-thumb:hover {
-  background: #555;
 }
 
 @media only screen and (max-width: 992px) {
-
-  #input {
-    margin-left: 40px;
+  #input-content {
+    left: 3em;
   }
 
   #weather {
-    margin-left: 0%; 
+    margin-left: 0%;
   }
 
- }
+  #view-content {
+    /* 
+      Leave space for the tab buttons down below. 
+      This is sensitive to the spacing defined on the tab buttons!
+    */
+    height: calc(100vh - 4.5em)
+  }
+
+  #input-content {
+    position: static;
+    max-height: none;
+    max-width: none;
+  }
+}
 
 @media only screen and (max-width: 600px) {
-  
-  #input {
-    margin-top: 550px;
-    margin-left: 0px;
-    max-height: none;
-  }
-
   #weather {
     position: absolute;
   }
@@ -180,12 +176,34 @@
   img {
     width: 50%;
   }
-
- }
+}
 
 /*
-  A basic little spinning circle from W3C:
-  https://www.w3schools.com/howto/howto_css_loader.asp
+  Scrollbar Styling
+  Currentl Chrome only.
+*/
+
+::-webkit-scrollbar {
+  width: 10px;
+}
+
+::-webkit-scrollbar-track {
+  background: #f1f1f1;
+  border-radius: var(--border-radius);
+}
+
+::-webkit-scrollbar-thumb {
+  background: #888;
+  border-radius: var(--border-radius);
+}
+
+::-webkit-scrollbar-thumb:hover {
+  background: #555;
+}
+
+/*
+  Loading symbol template
+  From: https://www.w3schools.com/howto/howto_css_loader.asp
 */
 
 .loader-template {
@@ -198,22 +216,27 @@
 }
 
 @keyframes loader-template-spin {
-  0% { transform: rotate(0deg); }
-  100% { transform: rotate(360deg); }
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
 }
 
-.btn-warning{
-  background-color: var(--border-color) !important;//overwrite the default color of bootstrap
+.btn-warning {
+  background-color: var(
+    --border-color
+  ) !important; //overwrite the default color of bootstrap
   border-color: var(--border-color) !important;
 }
-
 </style>
 
 <script>
 /* eslint-disable no-undef */
 import Map from "./components/Map";
-import TripSelection from "./components/TripSelection"
-import Weather from "./components/Weather"
+import TripSelection from "./components/TripSelection";
+import Weather from "./components/Weather";
 import DarkMode from "@/components/DarkMode";
 
 export default {
@@ -228,46 +251,58 @@ export default {
 
   data() {
     return {
-      journeyInfo: '<b>Journey Info</b>',
+      journeyInfo: "<b>Journey Info</b>",
       timestamp: null,
       selectedStop: null,
       isShow: true,
-      button_content: '>',
+      button_content: ">",
       root: null,
-      mode: 'light'
+      mode: "light",
     };
   },
 
-  methods:{
-    onClickViewButton:function (){
+  methods: {
+    onClickViewButton: function () {
       this.isShow = !this.isShow;
-      this.button_content = this.button_content==='>'?'<':'>';
+      this.button_content = this.button_content === ">" ? "<" : ">";
     },
 
-    showTripPrediction:function (route, direction, stop_dep, stop_arr, timestamp){
-      this.$refs.predict.getTripPrediction(route, direction, stop_dep, stop_arr, timestamp);
+    showTripPrediction: function (
+      route,
+      direction,
+      stop_dep,
+      stop_arr,
+      timestamp
+    ) {
+      this.$refs.predict.getTripPrediction(
+        route,
+        direction,
+        stop_dep,
+        stop_arr,
+        timestamp
+      );
     },
 
-    showGooglePrediction:function (route, timestamp){
+    showGooglePrediction: function (route, timestamp) {
       this.$refs.predict.getGooglePrediction(route, timestamp);
     },
 
-    updateTimestamp:function (val){
-      this.timestamp=val;
+    updateTimestamp: function (val) {
+      this.timestamp = val;
     },
 
     showStopTrips(stopID) {
       this.selectedStop = stopID;
     },
 
-    changeMode: function (mode){
-      this.mode=mode;
+    changeMode: function (mode) {
+      this.mode = mode;
     },
   },
 
   mounted() {
-    this.root=document.documentElement
-  }
-}
+    this.root = document.documentElement;
+  },
+};
 </script>
 
