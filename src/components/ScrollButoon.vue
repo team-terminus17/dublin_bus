49fdaed9573ca6f77db390289f4baf1e2c261919
ChristineCao98 <xiaoyan.cao@ucdconnect.ie>
2021-08-20 23:09:33 +0100

    Add scroll button for mobile view

---
+++ src/components/ScrollButoon.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="scroll">
+    <a href="#input-content">
+      <img :src=img_link[mode]>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ScrollButoon",
+
+  data(){
+    return{
+      img_link:{
+        'light':'https://img.icons8.com/ios-filled/50/347aa0/down-squared--v2.png',
+        'dark':'https://img.icons8.com/ios-filled/50/d59563/down-squared--v2.png'
+      },
+    }
+  },
+
+  props:['mode'],
+
+  computed: {
+    map() {
+      return this.$store.state.map;
+    },
+  },
+
+  watch:{
+  map(){
+    this.initAuto();
+  }
+  },
+
+  methods: {
+    initAuto: function () {
+      const controlDiv = document.getElementById("scroll");
+      this.map.controls[window.google.maps.ControlPosition.BOTTOM_CENTER].push(controlDiv);
+    },
+  }
+}
+</script>
+
+<style scoped>
+#scroll{
+  visibility: hidden;
+  margin: 20px 20px !important;
+}
+@media only screen and (max-width: 992px){
+  #scroll{
+    visibility: visible;
+  }
+}
+</style>
